7f2107fa42ecbd0627d5ebf1d043ad6e96e443c9
Mari Myrmoen <mari.myr@gmail.com>
2025-06-03 17:32:42 +0200

    docs: created Swagger-documentation (docs.yml) with menu, auth, cart and orders-endpoints

--- server.js
+++ server.js
@@ -7,29 +7,29 @@ import authRouter from "./routes/auth.js";
 import menuRouter from "./routes/menu.js";
 import cartRouter from "./routes/cart.js";
 import orderRouter from "./routes/order.js";
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
 
 // Config
-
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 mongoose.connect(process.env.CONNECTION_STRING);
 const database = mongoose.connection;
+const swaggerDocs = YAML.load("./docs/docs.yml");
 
 // Middlewares
-
 app.use(express.json());
 app.use(logger);
 
 // Routes
-
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use("/api/auth", authRouter);
 app.use("/api/menu", menuRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/orders", orderRouter);
 
 // DB
-
 database.on("error", (error) => console.log(error));
 database.once("connected", () => {
   console.log("DB Connected");
@@ -40,5 +40,4 @@ database.once("connected", () => {
 });
 
 // Error handling
-
 app.use(errorHandler);
